5b225f06769fdd73864878011473878aaed1e3e9
Jake Grella <jgrella13@gmail.com>
2020-11-04 21:27:04 -0600

    close to functional

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const AddedFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
-    </li>
-  );
+const AddedFeature = (props) => {
+	return (
+		<li>
+			{/* Add an onClick to run a function to remove a feature */}
+			<button
+				onClick={() => props.remvoeFeature(props.feature)}
+				className='button'
+			>
+				X
+			</button>
+			{props.feature.name}
+		</li>
+	);
 };
 
 export default AddedFeature;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+// libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// redux (helpers)
+import { reducer } from './store/reducers';
+
+// components
 import App from './App';
 
+// stylesheet
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
